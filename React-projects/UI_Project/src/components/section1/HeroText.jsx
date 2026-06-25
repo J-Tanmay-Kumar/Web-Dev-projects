11eac8c7e3f7f741d220f395cbdecf4449dcb00b
J TANMAY KUMAR <tanmaykumar9274@gmail.com>
2026-06-25 16:01:23 +0530

    rearranged the files in page1context

---
+++ React-projects/UI_Project/src/components/section1/HeroText.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='p-4'>
+                <h3 className='text-5xl font-bold mb-7'>Prospective <br>
+                </br><span className='text-gray-300'>
+                    customer
+                    </span>
+                    <br />
+                segementation</h3>
+                <p className='font-medium text-gray-500'>
+                    Depending upon on customer satisfaction and access to banking products,potential target Audience can be divided into three groups
+                </p>
+    </div>
+  )
+}
+
+export default HeroText
