65635d5d6ae5a795c88ca037a1d4448a686e2e88
J TANMAY KUMAR <tanmaykumar9274@gmail.com>
2026-07-04 19:28:41 +0530

    add mock product data array to product.js

---
+++ java_script/Shpping_cart/data/product.js
@@ -0,0 +1,62 @@
+export const products = [
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    price: 2499,
+    image: "headphones.png"
+  },
+  {
+    id: 2,
+    name: "Smartwatch Pro",
+    price: 4999,
+    image: "smartwatch.png"
+  },
+  {
+    id: 3,
+    name: "Bluetooth Speaker",
+    price: 1899,
+    image: "speaker.png"
+  },
+  {
+    id: 4,
+    name: "Mechanical Keyboard",
+    price: 3299,
+    image: "keyboard.png"
+  },
+  {
+    id: 5,
+    name: "Wireless Mouse",
+    price: 899,
+    image: "mouse.png"
+  },
+  {
+    id: 6,
+    name: "27-inch LED Monitor",
+    price: 12999,
+    image: "monitor.png"
+  },
+  {
+    id: 7,
+    name: "Portable Power Bank",
+    price: 1299,
+    image: "powerbank.png"
+  },
+  {
+    id: 8,
+    name: "USB-C Hub Adapter",
+    price: 1599,
+    image: "usb-hub.png"
+  },
+  {
+    id: 9,
+    name: "Noise Cancelling Earbuds",
+    price: 3499,
+    image: "earbuds.png"
+  },
+  {
+    id: 10,
+    name: "HD Webcam",
+    price: 2199,
+    image: "webcam.png"
+  }
+];
