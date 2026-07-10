be4fd5eca401be68926bbeeb24ead5e60998c576
J TANMAY KUMAR <tanmaykumar9274@gmail.com>
2026-07-11 00:37:21 +0530

    feat: implement basic layout for counter with history feature

--- React-projects/Counter-With-History/src/App.jsx
+++ React-projects/Counter-With-History/src/App.jsx
@@ -5,7 +5,15 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  
+  return (
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
+      <h1>Hello, World!</h1>
+      <p>This is a simple counter with history.</p>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        Click Me
+      </button>
+    </div>
+  )
 }
 
 export default App
