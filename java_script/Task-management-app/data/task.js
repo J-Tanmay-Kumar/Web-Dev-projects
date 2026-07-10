3b9c8c2ccb1692153eda8cdadf2f6ed78b0d4ba8
J TANMAY KUMAR <tanmaykumar9274@gmail.com>
2026-07-10 14:45:41 +0530

    Implement dynamic task rendering and add task data module

---
+++ java_script/Task-management-app/data/task.js
@@ -0,0 +1,18 @@
+export const tasks = [
+  {
+    id: 1,
+    text: "Fix the porch light switch",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "Sand the back door frame",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Order replacement hinges",
+    completed: false
+  }
+];
+
