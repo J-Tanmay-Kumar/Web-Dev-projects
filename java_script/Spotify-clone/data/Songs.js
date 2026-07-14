e78665f8c6220ed4015626d187b9830c00fe96ec
J TANMAY KUMAR <tanmaykumar9274@gmail.com>
2026-07-14 16:54:04 +0530

    Add "Young And Beautiful" by Lana Del Rey to the song library
    - Added audio file for "Young And Beautiful" (Lana Del Rey)
    - Created new Songs.js file to include song metadata
    - Included song details such as title, artist, album, duration, cover image, and audio path

---
+++ java_script/Spotify-clone/data/Songs.js
@@ -0,0 +1,14 @@
+
+
+export const songs = 
+[
+    {
+        id: 1,
+        title: "young and beautiful",
+        artist: "lana Del Rey",
+        album: "The great Gatsby",
+        duration: "3:56",
+        cover: "covers/lana del rey - young and beautiful.jpg",
+        audio:"songs/lana del rey - young and beautiful.mp3"
+    }
+]
--- java_script/Spotify-clone/script.js
+++ java_script/Spotify-clone/script.js
@@ -1,1 +1,9 @@
+import { songs } from "./data/Songs.js";
+
+
 console.log("Welcome to Ember Web Player");
+
+
+songs.forEach((song) => {
+    console.log(`Song: ${song.title}, Artist: ${song.artist}, Album: ${song.album}, Duration: ${song.duration}`);
+})
