5558db8b58f6930faac1e5a199b6138c7efa47ab
Tanmay Kumar <tanmaykumar9274@gmail.com>
2026-08-09 20:00:26 +0530

    Add new media assets for Spotify clone
    - Added cover image for The Weeknd.
    - Added audio files for Justin Bieber's "Company", The Weeknd's "Society", and Selena Gomez & Marshmello's "Wolves".

--- java_script/Spotify-clone/data/Songs.js
+++ java_script/Spotify-clone/data/Songs.js
@@ -8,5 +8,32 @@ export const songs =
         duration: "3:56",
         cover: "assets/covers/lana-young-and-beautiful.jpg",
         audio:"assets/songs/Young And Beautiful.mp3"
+    },
+    {
+        id: 2,
+        title: "Society",
+        artist: "The Weekend",
+        album: "The great Gatsby",
+        duration: "2:54",
+        cover: "assets/covers/theweekend.jpg",
+        audio:"assets/songs/The Weeknd - Society.mp3"
+    },
+    {
+        id: 3,
+        title: "Wolves",
+        artist: "Selena Gomez",
+        album: "The great Gatsby",
+        duration: "3:32",
+        cover: "assets/covers/selena.jpg",
+        audio:"assets/songs/selena-gomez-marshmello-wolves.mp3"
+    },
+    {
+        id: 4,
+        title: "Company",
+        artist: "Justin Bieber",
+        album: "The great Gatsby",
+        duration: "3:27",
+        cover: "assets/covers/Company_(Justin_Bieber_song).jpg",
+        audio:"assets/songs/Justin Bieber - Company.mp3"
     }
 ]
